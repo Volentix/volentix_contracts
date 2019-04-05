b18a633994cecaf488e2e254da3b429dbfc50df8
maxpetriev <maxpetriev@gmail.com>
2019-04-05 21:41:16 +0300

    volentixpool refact

---
+++ volentixpool/src/volentixpool.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/asset.hpp>
+#include <eosiolib/symbol.hpp>
+#include <eosiolib/name.hpp>
+
+#include <vector>
+
+using namespace eosio;
+using std::vector;
+
+using namespace eosio;
+
+class [[eosio::contract]] volentixpool : public eosio::contract {
+
+    public:
+        using contract::contract;
+        volentixpool(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
+
+        const name treasury = name("staider11111");
+        const name vtxsys_contract = name("volentixgsys");
+  
+
+        [[eosio::action]]
+        void payproducer(name account, asset quantity);
+
+        [[eosio::action]]
+        void payliquid(name account, asset quantity);
+};
