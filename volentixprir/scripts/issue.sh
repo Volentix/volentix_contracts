b0f9860d6d001d2b10dbf91bca3600a1fa6e96d3
sylvain <sylvaincormier@protonmail.com>
2019-07-14 12:46:08 -0400

    issuance scripts

--- volentixprir/scripts/issue.sh
+++ volentixprir/scripts/issue.sh
@@ -1,7 +1,7 @@
 url='https://api.eosnewyork.io:443'
 
 # RENAME PROPOSAL NAME, TO AND QUANTITY
-cleos -u $url multisig propose issue '[{"actor": "staider11111", "permission": "active"},{"actor": "volentixdev2", "permission": "active"}]' '[{"actor": "volentixgsys", "permission": "active"}]' volentixgsys issue '{"to": "volentixpool", "quantity": "800000000.00000000 VTX", "memo": ""}' -p volentixdev2@active;
+cleos -u $url multisig propose issue '[{"actor": "staider11111", "permission": "active"},{"actor": "volentixdev2", "permission": "active"}]' '[{"actor": "volentixgsys", "permission": "active"}]' volentixgsys issue '{"to": "volentixprir", "quantity": "130000000.00000000 VTX", "memo": ""}' -p volentixdev2@active;
 cleos -u $url multisig approve volentixdev2 issue '{"actor": "staider11111", "permission": "active"}' -p staider11111@active;
 cleos -u $url multisig approve volentixdev2 issue '{"actor": "volentixdev2", "permission": "active"}' -p volentixdev2@active;
 cleos -u https://api.eosnewyork.io:443 multisig exec volentixdev2 issue -p volentixdev2@active;
